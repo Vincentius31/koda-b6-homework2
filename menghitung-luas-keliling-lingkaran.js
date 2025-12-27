b21dd94cd5a0ce09bcfa0984e63a930e72e65f4b
Vincentius Dwi Prasetyo <62978348+Vincentius31@users.noreply.github.com>
2025-12-27 08:53:47 +0700

    Add files via upload

---
+++ menghitung-luas-keliling-lingkaran.js
@@ -0,0 +1,25 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.question("Masukan jari-jari: ", (input) => {
+  const r = Number(input);
+  let phi;
+  
+  if(r % 7 === 0){ //Kondisi untuk penggunaan phi
+    phi = 22/7
+  }
+  else{
+    phi = 3.14
+  }
+
+  const luasLingkaran = phi * r * r;
+  const kelilingLingkaran = 2 * phi * r;
+
+  console.log(`Luas Lingkaran: ${luasLingkaran}`)
+  console.log(`Keliling Lingkaran: ${kelilingLingkaran}`)
+  rl.close()
+});
